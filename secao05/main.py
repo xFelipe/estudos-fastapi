03cd1c68af75789a28974b7853c1e6a7a6d8463a
Felipe Gomes Duarte <xfelipegd@gmail.com>
2024-08-27 17:13:16 -0300

    Log level info

--- secao05/main.py
+++ secao05/main.py
@@ -8,4 +8,4 @@ app.include_router(router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True, log_level="info")
